ebc0c33e89db01e794b889d615f4f1bfa9d46547
Victor Kauan <victorkauan.dev@gmail.com>
2023-11-15 17:18:44 -0300

    :sparkles: feat: Criar serviço e grupo de rotas de logs (auditoria)

--- src/routes/log.py
+++ src/routes/log.py
@@ -1,39 +1,68 @@
-from flask import request, jsonify
+from flask import request
 
+from src.services import log as log_service
 from src.routes import log_blueprint
-
-items = []
 
 
 @log_blueprint.route(rule="/", methods=["GET"])
-def get_logs(data):
-    return jsonify(data)
+def index():
+    found_logs = [result.to_dict() for result in log_service.get_all()]
+
+    return {"message": "Logs found successfully.", "logs": found_logs}, 200
+
+
+@log_blueprint.route(rule="/<id>", methods=["GET"])
+def view(id):
+    found_log = log_service.get_by_id(id=id)
+
+    if not found_log:
+        return {"message": f"Log with ID '{id}' not found."}, 404
+
+    found_log = found_log.to_dict()
+
+    return {"message": "Log found successfully.", "log": found_log}, 200
 
 
 @log_blueprint.route(rule="/", methods=["POST"])
-def post_log():
-    data = request.get_json()
-    response = {"message": "Dados enviados com sucesso"}
-    return jsonify(response)
+def create():
+    body = request.get_json()
+
+    if "message" not in body.keys():
+        return {"error": "Invalid payload", "message": "Please provide the required log fields."}, 400
+
+    message = body["message"]
+
+    new_log = log_service.create(message=message).to_dict()
+
+    return {"message": "Log created successfully.", "log": new_log}, 201
 
 
 @log_blueprint.route(rule="/<id>", methods=["PUT", "PATCH"])
-def put_log(id):
-    data = request.get_json()
-    if id < len(items):
-        items[id] = data
-        response = {"message": "Log atualizado com sucesso."}
-    else:
-        response = {"error": "Log não encontrado."}
-    return jsonify(response)
+def update(id):
+    body = request.get_json()
+
+    if "message" not in body.keys():
+        return {"error": "Invalid payload", "message": "Please provide the required log fields."}, 400
+
+    found_log = log_service.get_by_id(id=id)
+
+    if not found_log:
+        return {"message": f"Log with ID '{id}' not found."}, 404
+
+    message = body["message"]
+
+    found_log = log_service.update(log=found_log, message=message).to_dict()
+
+    return {"message": "Log updated successfully.", "log": found_log}, 200
 
 
 @log_blueprint.route(rule="/<id>", methods=["DELETE"])
-def delete_log(id):
-    for item in items:
-        if item["id"] == id:
-            items.remove(item)
-            response = {"message": "Log excluído com sucesso."}
-            return jsonify(response)
-    response = {"error": "Log não encontrado."}
-    return jsonify(response)
+def destroy(id):
+    found_log = log_service.get_by_id(id=id)
+
+    if not found_log:
+        return {"message": f"Log with ID '{id}' not found."}, 404
+
+    log_service.delete(log=found_log)
+
+    return {}, 204
