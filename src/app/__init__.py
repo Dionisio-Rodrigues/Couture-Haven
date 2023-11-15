037851159dfe207cbb0cac91c20694187107c6d1
Victor Kauan <victorkauan.dev@gmail.com>
2023-11-15 16:26:59 -0300

    :sparkles: feat: Criar serviço de categorias

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -7,19 +7,19 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///dev.db"
 
 db = SQLAlchemy(app)
 
-from src.models import category
+from src.services import category as category_service
 
 
 @app.route(rule="/category", methods=["GET"])
 def index():
-    found_categories = [result.to_dict() for result in category.Category.query.all()]
+    found_categories = [result.to_dict() for result in category_service.get_all()]
 
     return {"message": "Categories found successfully.", "categories": found_categories}, 200
 
 
 @app.route(rule="/category/<id>", methods=["GET"])
 def view(id):
-    found_category = category.Category.query.filter_by(id=id).first()
+    found_category = category_service.get_by_id(id=id)
 
     if not found_category:
         return {"message": f"Category with ID '{id}' not found."}, 404
@@ -38,15 +38,10 @@ def create():
 
     name = body["name"]
 
-    if category.Category.query.filter_by(name=name).first():
+    if category_service.get_by_name(name=name):
         return {"error": "Category already exists", "message": "A category with provided name already exists."}, 409
 
-    new_category = category.Category(name=name)
-
-    db.session.add(new_category)
-    db.session.commit()
-
-    new_category = new_category.to_dict()
+    new_category = category_service.create(name=name).to_dict()
 
     return {"message": "Category created successfully.", "category": new_category}, 201
 
@@ -58,34 +53,28 @@ def update(id):
     if "name" not in body.keys():
         return {"error": "Invalid payload", "message": "Please provide the required category fields."}, 400
 
-    found_category = category.Category.query.filter_by(id=id).first()
+    found_category = category_service.get_by_id(id=id)
 
     if not found_category:
         return {"message": f"Category with ID '{id}' not found."}, 404
 
     name = body["name"]
 
-    if category.Category.query.filter(category.Category.id != id).filter_by(name=name).first():
+    if category_service.get_by_name(name=name, exception_id=id):
         return {"error": "Category already exists", "message": "A category with provided name already exists."}, 409
 
-    found_category.name = name
-
-    db.session.add(found_category)
-    db.session.commit()
-
-    found_category = found_category.to_dict()
+    found_category = category_service.update(category=found_category, name=name).to_dict()
 
     return {"message": "Category updated successfully.", "category": found_category}, 200
 
 
 @app.route(rule="/category/<id>", methods=["DELETE"])
 def destroy(id):
-    found_category = category.Category.query.filter_by(id=id).first()
+    found_category = category_service.get_by_id(id=id)
 
     if not found_category:
         return {"message": f"Category with ID '{id}' not found."}, 404
 
-    db.session.delete(found_category)
-    db.session.commit()
+    category_service.delete(category=found_category)
 
     return {}, 204
