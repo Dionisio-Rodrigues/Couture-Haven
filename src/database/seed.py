d3bb6b5abdf71199cd37f73857bf6ffb25bd6230
Victor Kauan <victorkauan.dev@gmail.com>
2023-11-20 21:55:01 -0300

    :sparkles: feat: Criar script para popular produtos no banco de dados

--- src/database/seed.py
+++ src/database/seed.py
@@ -9,14 +9,14 @@ CATEGORIES = [
     {"name": "Bermudas"},
 ]
 
-# PRODUCTS_DATA = [
-#     {"id": 1, "name": "Camisa polo branca", "price": 79.99, "category_id": 1},
-#     {"id": 2, "name": "Camisa careca cinza", "price": 49.99, "category_id": 1},
-#     {"id": 3, "name": "Casaco zíper azul", "price": 139.99, "category_id": 2},
-#     {"id": 4, "name": "Casaco simples amarelo", "price": 89.99, "category_id": 2},
-#     {"id": 5, "name": "Calça jeans azul", "price": 124.99, "category_id": 3},
-#     {"id": 6, "name": "Calça brim preta", "price": 69.99, "category_id": 3},
-# ]
+PRODUCTS = [
+    {"name": "Camisa polo branca", "price": 79.99, "category_id": 1},
+    {"name": "Camisa careca cinza", "price": 49.99, "category_id": 1},
+    {"name": "Casaco zíper azul", "price": 139.99, "category_id": 2},
+    {"name": "Casaco simples amarelo", "price": 89.99, "category_id": 2},
+    {"name": "Calça jeans azul", "price": 124.99, "category_id": 3},
+    {"name": "Calça brim preta", "price": 69.99, "category_id": 3},
+]
 
 
 def seed():
@@ -25,6 +25,15 @@ def seed():
     print("INSERTING CATEGORY(IES) INTO DATABASE...")
     [post(url=f"{BASE_API_URL}/category", json={"name": category["name"]}) for category in CATEGORIES]
     print(f"[SUCCESS] {len(CATEGORIES)} CATEGORY(IES) INSERTED.")
+
+    print("INSERTING PRODUCT(S) INTO DATABASE...")
+    [
+        post(
+            url=f"{BASE_API_URL}/product",
+            json={"name": product["name"], "price": product["price"], "category_id": product["category_id"]},
+        ) for product in PRODUCTS
+    ]
+    print(f"[SUCCESS] {len(PRODUCTS)} PRODUCT(S) INSERTED.")
 
     print("[SUCCESS] SEED COMPLETE.")
 
