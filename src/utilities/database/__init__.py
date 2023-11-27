d13566890240b702d0e97745a8eaa3750fc17c34
Victor Kauan <victorkauan.dev@gmail.com>
2023-11-26 22:14:17 -0300

    :sparkles: feat: Adicionar recursão à função formatar condições

--- src/utilities/database/__init__.py
+++ src/utilities/database/__init__.py
@@ -1,1 +1,6 @@
-format_conditions = lambda dictionary: [f"{key}: {value}" for key, value in dictionary.items()]
+format_conditions = lambda conditions: "[{}]".format(", ".join(
+    [
+        "{}: {}".format(key, f"({format_conditions(value)})" if isinstance(value, dict) else value)
+        for key, value in conditions.items()
+    ]
+))
